afe59556beec966b3954ccdfdbf83aa3c53f6aa6
Ariane Chagas <ariane0.gc@gmail.com>
2025-08-21 19:54:58 -0300

    Aula 21-08

--- 21-08/exemplo.1.php
+++ 21-08/exemplo.1.php
@@ -3,18 +3,18 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title></title>
+<title>Exemplo - Soma de Valores</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container">
-<h1></h1>
-<form method="post" action="FormularioSomaResp.php"> 
+<h1>Exemplo - Soma de Valores</h1>
+<form method="post" action="resposta_exemplo1.php">
 <div class="mb-3">
-              <label for="valor1" class="form-label">Valor 1</label>
+              <label for="valor1" class="form-label">Informe o primeiro valor</label>
               <input type="number" id="valor1" name="valor1" class="form-control" required="">
             </div><div class="mb-3">
-              <label for="valor2" class="form-label">Valor 2</label>
+              <label for="valor2" class="form-label">Informe o segundo valor</label>
               <input type="number" id="valor2" name="valor2" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
