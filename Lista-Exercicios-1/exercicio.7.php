23b4426082cc1e848bdf09008411fbfc393564f5
Ariane Chagas <ariane0.gc@gmail.com>
2025-08-22 21:22:13 -0300

    Exercício 6 e 7

--- Lista-Exercicios-1/exercicio.7.php
+++ Lista-Exercicios-1/exercicio.7.php
@@ -8,25 +8,25 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 6</title>
+<title>Exercício 7</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container">
-<h1>Conversão de °C para °F</h1>
+<h1>Conversão de °F para °C</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="temperatura1" class="form-label">Insira uma temperatura em °C</label>
-              <input type="number" id="temperatura1" name="temperatura1" class="form-control" required="">
+              <label for="temperatura" class="form-label">Insira uma temperatura em °F</label>
+              <input type="number" id="temperatura" name="temperatura" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
-
+(20 °F − 32) × 5/9
 <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-      $temperatura1 = $_POST['temperatura1'];
-      $f = ($temperatura1 * 1.8) + 32;
-      echo "<p> $temperatura1 °C = $f °F</p>";
+      $temperatura = $_POST['temperatura'];
+      $f = ($temperatura * 1.8) + 32;
+      echo "<p> $temperatura °F = $f °C</p>";
     }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
