9aa0857b3f21e2b60432e57ecce656b8e4e10e17
Ariane Chagas <ariane0.gc@gmail.com>
2025-08-28 21:02:16 -0300

    Aula 28-08

---
+++ 28-08/estrutura3.php
@@ -0,0 +1,20 @@
+<?php
+
+    include("cabecalho.php");
+
+    //1-Domingo  2-Segunda...
+    $diaSemana = 3;
+
+    switch($diaSemana){
+        case 1:
+            echo "Hoje é domingo!";
+            break;
+        case 2:
+            echo "Hoje é segunda!";
+            break;
+        case 3: 
+            echo "Hoje é terça!";
+            break;
+        default:
+        echo "Hoje pode se quarta, quinta, sexta ou sábado!";
+    }
---
+++ 28-08/estrutura4.php
@@ -0,0 +1,11 @@
+<?php
+    include("cabecalho.php");
+
+    $i = 1;
+
+    while($i <= 4){
+        echo "<p>Número $i</p>";
+        $i++;
+    }
+
+    include("rodape.php");
