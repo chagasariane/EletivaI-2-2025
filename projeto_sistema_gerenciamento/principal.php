3030f97fe84949434c1f70245f1183c097b800fa
Ariane Chagas <ariane0.gc@gmail.com>
2025-11-05 20:21:02 -0300

    Projeto 05-11

--- projeto_sistema_gerenciamento/principal.php
+++ projeto_sistema_gerenciamento/principal.php
@@ -1,41 +1,8 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>LAR AMIGO</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-  <div class="container">
-    <a class="navbar-brand" href="#">LAR AMIGO</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-<li class="nav-item">
-  <a class="nav-link" aria-current="page" href="#">Home</a>
-</li>
-
-<li class="nav-item dropdown">
-  <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Cadastros
-  </a>
-  <ul class="dropdown-menu" aria-labelledby="dropdown2">
-    <li><a class="dropdown-item" href="#">Animal</a></li>
-<li><a class="dropdown-item" href="#">Adotante</a></li>
-<li><a class="dropdown-item" href="#">ONG</a></li>
-  </ul>
-</li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<div class="container py-3">
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+    require("cabecalho.php");
+?>
+    <h1>Seja Bem Vindos ao Lar Amigo <?= $_SESSION['nome'] ?></h1>
+    <h6><a href="logout.php">Sair</a></h6>
+<?php
+  require("rodape.php");
+?>
